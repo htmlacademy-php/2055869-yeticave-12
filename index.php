7695c7202f8faa94937df3242df438b871faffa7
erma4ina <ermoliano@gmail.com>
2022-04-25 22:41:15 +0300

    выполнил задание 1.13 Работа с условиями

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'erma4ina krasava molorik uabshet'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -28,6 +28,25 @@ $user_name = ''; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
+        <?php
+        if ($is_auth == 1) { ?>
+            <div class="user-menu__logged">
+                <p>erma4ina krasava molorik uabshet</p>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__logout" href="#">Выход</a>
+            </div> 
+        <?php } else { ?>
+            <ul class="user-menu__list">
+             <li class="user-menu__item">
+              <a href="#">Регистрация</a>
+             </li>
+             <li class="user-menu__item">
+              <a href="#">Вход</a>
+             </li>
+            </ul>
+        <?php }
+        ?>
+        
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
         </nav>
