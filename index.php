6e0e2c0dab46e383af28de2bb8a9b3a43e0f61e9
erma4ina <ermoliano@gmail.com>
2022-05-08 21:57:10 +0300

    Добавил вывод объявлений из массива с товарами + время к ним

--- index.php
+++ index.php
@@ -124,24 +124,26 @@ $lots = [
                 </div>
                 <ul class="lots__list">
                     <!--заполните этот список из массива с товарами-->
-                    <li class="lots__item lot">
-                        <div class="lot__image">
-                            <img src="" width="350" height="260" alt="">
-                        </div>
-                        <div class="lot__info">
-                            <span class="lot__category">Название категории</span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost">цена<b class="rub">р</b></span>
-                                </div>
-                                <div class="lot__timer timer">
-                                    12:23
+                    <?php foreach ($lots as $key => $item) { ?>
+                        <li class="lots__item lot">
+                            <div class="lot__image">
+                                <img src="<?= $item['url'] ?>" width="350" height="260" alt="">
+                            </div>
+                            <div class="lot__info">
+                                <span class="lot__category"><?= $item['category'] ?></span>
+                                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['name'] ?></a></h3>
+                                <div class="lot__state">
+                                    <div class="lot__rate">
+                                        <span class="lot__amount"><?= $item['price'] ?></span>
+                                        <span class="lot__cost">цена<b class="rub">р</b></span>
+                                    </div>
+                                    <div class="lot__timer timer">
+                                        <?= $currentTime ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
+                        </li>
+                    <?php } ?>
                 </ul>
             </section>
         </main>
