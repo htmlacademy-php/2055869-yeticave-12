d56e0666d78891cc4bf6ed50c73235daef00702e
erma4ina <ermoliano@gmail.com>
2022-05-25 13:39:01 +0300

    создал шаблоны layout.php и main.php, выполнил вывод главной страницы index.php с использованием шаблонизации

---
+++ templates/main.php
@@ -0,0 +1,40 @@
+<main class="container">
+    <section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <?php foreach ($categories as $cssClass => $categoryName) { ?>
+                <li class="promo__item <?= $cssClass ?>">
+                    <a class="promo__link" href="pages/all-lots.html"><?= $categoryName; ?></a>
+                </li>
+            <?php } ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <?php foreach ($lots as $key => $item) { ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?= $item['url'] ?>" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?= $item['category'] ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['name'] ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?= lotPrice($item['price']) ?></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                <?= $currentTime ?>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+    </section>
+</main>
