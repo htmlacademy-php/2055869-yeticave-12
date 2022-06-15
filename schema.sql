e4091b654ee22c1f922686e4f4b2285bbe8eeaa5
erma4ina <ermoliano@gmail.com>
2022-06-15 16:59:20 +0300

    произведена доработка задания по замечаниям: добавлены автогенерация первичных ключей, индексация строолбца name таблицы user, поясняющие комментарии к столбцам.

--- schema.sql
+++ schema.sql
@@ -1,42 +1,56 @@
 CREATE DATABASE IF NOT EXISTS YetiCave
-    DEFAULT CHARACTER SET utf8mb4
-    DEFAULT COLLATE utf8mb4_unicode_ci;
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
 USE YetiCave;
 CREATE TABLE IF NOT EXISTS category (
-    id INT PRIMARY KEY,
-    name VARCHAR(128) UNIQUE NOT NULL,
-    symbol_code VARCHAR(128) UNIQUE NOT NULL
+  id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name        VARCHAR(128) UNIQUE NOT NULL
+  COMMENT 'название',
+  symbol_code VARCHAR(128) UNIQUE NOT NULL
+  COMMENT 'символьный код'
 );
 CREATE TABLE IF NOT EXISTS user (
-    id INT PRIMARY KEY,
-    date_of_registration DATETIME(6) NOT NULL,
-    email VARCHAR(128) UNIQUE NOT NULL,
-    name VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    contacts VARCHAR(128)
+  id                   INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  date_of_registration DATETIME            NOT NULL
+  COMMENT 'дата регистрации пользователя',
+  email                VARCHAR(128) UNIQUE NOT NULL
+  COMMENT 'email пользователя',
+  name                 VARCHAR(128)        NOT NULL
+  COMMENT 'имя пользователя',
+  password             VARCHAR(128)        NOT NULL
+  COMMENT 'пароль пользователя',
+  contacts             VARCHAR(128) COMMENT 'контактная информация',
+  INDEX `name` (`name`) USING BTREE
 );
 CREATE TABLE IF NOT EXISTS lot (
-    id INT PRIMARY KEY,
-    date_of_create DATETIME(6) NOT NULL,
-    title VARCHAR(128) NOT NULL,
-    description TEXT,
-    img VARCHAR(128),
-    start_price INT NOT NULL,
-    expiration_date DATE NOT NULL,
-    bet_step INT NOT NULL,
-    category_id INT NOT NULL,
-    winner_id INT,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (winner_id) REFERENCES user(id),
-    FOREIGN KEY (category_id) REFERENCES category(id)
+  id              INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  date_of_create  DATETIME     NOT NULL
+  COMMENT 'дата создания лота',
+  title           VARCHAR(128) NOT NULL
+  COMMENT 'название лота',
+  description     TEXT COMMENT 'описание лота',
+  img             VARCHAR(128) COMMENT 'ссылка на изображение лота',
+  start_price     INT          NOT NULL
+  COMMENT 'стартовая цена лота',
+  expiration_date DATE         NOT NULL
+  COMMENT 'дата истечения нахождения лота на сайте',
+  bet_step        INT          NOT NULL
+  COMMENT 'шаг ставки',
+  category_id     INT UNSIGNED NOT NULL,
+  winner_id       INT UNSIGNED NOT NULL,
+  user_id         INT UNSIGNED NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (winner_id) REFERENCES user (id),
+  FOREIGN KEY (category_id) REFERENCES category (id)
 );
 CREATE TABLE IF NOT EXISTS bet (
-    id INT PRIMARY KEY,
-    user_id INT NOT NULL,
-    date DATETIME(6) NOT NULL,
-    price INT NOT NULL,
-    lot_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (lot_id) REFERENCES lot(id)
+  id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  date    DATETIME     NOT NULL
+  COMMENT 'дата и время размещения ставки',
+  price   INT          NOT NULL
+  COMMENT 'цена, по которой пользователь готов приобрести лот',
+  lot_id  INT UNSIGNED NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (lot_id) REFERENCES lot (id)
 );
