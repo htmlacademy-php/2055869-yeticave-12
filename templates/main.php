860e0103e44ed542446ceaba2db2ee2e9735fa18
erma4ina <ermoliano@gmail.com>
2022-12-01 18:34:37 +0500

    корректировка переменных в шаблоне main

--- templates/main.php
+++ templates/main.php
@@ -3,9 +3,9 @@
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <?php foreach ($categories as $cssClass => $categoryName) { ?>
-                <li class="promo__item <?= XSSfiltr($cssClass) ?>">
-                    <a class="promo__link" href="pages/all-lots.html"><?= XSSfiltr($categoryName) ?></a>
+            <?php foreach ($categories as $key => $row) { ?>
+                <li class="promo__item promo__item--<?= XSSfiltr($row['symbol_code']) ?>">
+                    <a class="promo__link" href="pages/all-lots.html"><?= XSSfiltr($row['name']) ?></a>
                 </li>
             <?php } ?>
         </ul>
@@ -17,7 +17,7 @@
         <ul class="lots__list">
             <?php foreach ($lots as $key => $item) {
                 $class = '';
-                $time = timeLeft($item['dateLeft']);
+                $time = timeLeft($item['expiration_date']);
                 if ($time === false) {
                     continue;
                 }
@@ -29,16 +29,16 @@
                 ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?= XSSfiltr($item['url']) ?>" width="350" height="260" alt="">
+                        <img src="<?= XSSfiltr($item['img']) ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?= XSSfiltr($item['category']) ?></span>
+                        <span class="lot__category"><?= XSSfiltr($item['name']) ?></span>
                         <h3 class="lot__title"><a class="text-link"
-                                                  href="pages/lot.html"><?= XSSfiltr($item['name']) ?></a></h3>
+                                                  href="pages/lot.html"><?= XSSfiltr($item['title']) ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?= lotPrice(XSSfiltr($item['price'])) ?></span>
+                                <span class="lot__cost"><?= lotPrice(XSSfiltr($item['start_price'])) ?></span>
                             </div>
                             <div class="lot__timer timer <?= $class ?>">
                                 <?= $time['hours'] . ': ' . $time['minutes'] ?>
