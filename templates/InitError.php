6ad63126b5534824a4f55b93d0a06ec2ae48df17
erma4ina <ermoliano@gmail.com>
2022-12-02 11:02:39 +0500

    создаю шаблон для ошибки подключения к БД

---
+++ templates/InitError.php
@@ -0,0 +1,6 @@
+<main class="container">
+    <section class="promo">
+        <h2 class="promo__title">ошибка соединения, братишка! Усе погано!</h2>
+        <p class="promo__text">данные об ошибке</p>
+    </section>
+</main>
